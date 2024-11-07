4c10f73c14602f5d7c0bc8b975de8665a2edaf7c
LIAM IMRAY <D3989146@live.tees.ac.uk>
2024-11-07 15:18:19 +0000

    Add changes to CateringDbContext.cs

--- ThAmCo.Catering/Data/CateringDbContext.cs
+++ ThAmCo.Catering/Data/CateringDbContext.cs
@@ -7,6 +7,35 @@ namespace ThAmCo.Catering.Data
         public DbSet<FoodBooking> FoodBookings { get; set; }
         public DbSet<FoodItem> FoodItems { get; set; }
         public DbSet<MenuFoodItem> MenuFoodItems { get; set; }
-        public DbSet<Menu>  Menu { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+
+        private readonly IHostEnvironment _hostEnv;
+        private readonly string DbPath;
+
+        public CateringDbContext(DbContextOptions<CateringDbContext> options, IHostEnvironment env) : base(options)
+        {
+            _hostEnv = env;
+
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "ThAmCo.Venues.db");
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Menu>()
+                   .HasKey(m => new { m.MenuId });
+
+        }
     }
 }
